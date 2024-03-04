311432db5a543bfd206f5590abb26776dc498649
jiiijb <150182767+jiiijb@users.noreply.github.com>
2024-03-04 15:30:38 +0000

    user

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: false,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
-  }
+    admin: 'hjkllk',
+    admin2: 'asdffd',
+  },
 }
 export default config
